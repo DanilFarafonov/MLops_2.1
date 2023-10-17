1590a9e4b6721379783a4e498aca41902cfbc3ee
danil <danil@ml-srv.myguest.virtualbox.org>
2023-10-17 22:47:31 +0500

    добавляет возможность указания гиперпараметров для модели

--- scripts/model_creation.py
+++ scripts/model_creation.py
@@ -1,19 +1,27 @@
 import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
+import yaml
 
+
+# считываем гиперпараметры модели
+params = yaml.safe_load(open("scripts/params.yaml"))["model"]
+
+# загружаем тренировочные данные
 x_train = pd.read_csv("data/x_train.csv")
 y_train = pd.read_csv("data/y_train.csv")
 
+# создание нейронной сети
 model = Sequential()
 model.add(Dense(800, input_dim=784, activation="relu"))
 model.add(Dense(10, activation="softmax"))
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-print(model.summary())
-
+# обучение нейронной сети с указанными гиперпараметрами
 model.fit(x_train, y_train,
-          batch_size=200,
-          epochs=10,
-          verbose=1)
+          batch_size=params["batch_size"],
+          epochs=params["epochs"],
+          verbose=params["verbose"])
+
+# сохранение модели
 model.save('model')
--- scripts/model_testing.py
+++ scripts/model_testing.py
@@ -2,15 +2,15 @@ import pandas as pd
 from tensorflow import keras
 import json
 
-
+# загружаем тестовые данные
 x_test = pd.read_csv('data/x_test.csv')
 y_test = pd.read_csv('data/y_test.csv')
 
+# загружаем модель и запускаем тестирование
 model = keras.models.load_model('model')
 score = model.evaluate(x_test, y_test)
 
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+# сохранение результатов эксперимента
 results = {'loss': score[0], "accuracy": score[1]}
 with open("results.json", "w") as file:
     json.dump(results, file)
